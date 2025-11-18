96f684fa9ffe259b0c24d719956c945c12c66528
ABHISHEKyadav281 <yyadavabhishek9@gmail.com>
2025-11-18 23:51:59 +0530

    experience section scroll fix

--- src/Pages/MainPage/MainPage.tsx
+++ src/Pages/MainPage/MainPage.tsx
@@ -1,3 +1,77 @@
+// import ExperienceSection from "../../components/Experience/ExperienceSection";
+// import Peach3DBackground from "../../components/Peach3DBackground/Peach3DBackground";
+// import ProjectGalMain from "../../components/ProjectGallery/ProjectGalMain/ProjectGalMain";
+// import TechMarquee from "../../components/TechMarquee/TechMarquee";
+// import { useTheme } from "../../Wrappers/ThemeProvider/ThemeProviderWrapper";
+// import About from "../AboutPage/About";
+// import Home from "../HomePage/Home";
+// import LetsConnect from "../LetsConnect/LetsConnect";
+
+// const MainPage = () => {
+//   const theme = useTheme();
+//   return (
+//     <>
+//       <div
+//         className="min-h-screen w-screen relative "
+//         style={{ backgroundColor: theme.background.default }}
+//       >
+        
+//         <div className="fixed w-full h-full top-0 left-0 z-0">
+//           <Peach3DBackground />
+//         </div>
+
+//         <div className="relative z-10">
+//           <section id="home">
+//             <Home />
+//             <div>
+//               <TechMarquee />
+//             </div>
+//           </section>
+//           <section id="experience">
+//             <ExperienceSection />
+//           </section>
+//           <section id="projects">
+//             <ProjectGalMain />
+//           </section>
+//           <section id="about">
+//             <About />
+//           </section>
+//           <section id="contact">
+//             <LetsConnect />
+//           </section>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+// export default MainPage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import ExperienceSection from "../../components/Experience/ExperienceSection";
 import Peach3DBackground from "../../components/Peach3DBackground/Peach3DBackground";
 import ProjectGalMain from "../../components/ProjectGallery/ProjectGalMain/ProjectGalMain";
@@ -12,31 +86,31 @@ const MainPage = () => {
   return (
     <>
       <div
-        className="min-h-screen w-screen relative "
+        className="min-h-screen w-full max-w-full overflow-x-hidden relative"
         style={{ backgroundColor: theme.background.default }}
       >
         
-        <div className="fixed w-full h-full top-0 left-0 z-0">
+        <div className="fixed w-full h-full top-0 left-0 z-0 overflow-hidden">
           <Peach3DBackground />
         </div>
 
-        <div className="relative z-10">
-          <section id="home">
+        <div className="relative z-10 w-full max-w-full">
+          <section id="home" className="w-full">
             <Home />
-            <div>
+            <div className="w-full">
               <TechMarquee />
             </div>
           </section>
-          <section id="experience">
+          <section id="experience" className="w-full">
             <ExperienceSection />
           </section>
-          <section id="projects">
+          <section id="projects" className="w-full">
             <ProjectGalMain />
           </section>
-          <section id="about">
+          <section id="about" className="w-full">
             <About />
           </section>
-          <section id="contact">
+          <section id="contact" className="w-full">
             <LetsConnect />
           </section>
         </div>
